310232c7339ae6e983057b139556240efea450b0
Emma Paczkowski <emmapaczkowski@gmail.com>
2020-06-05 00:47:24 +0000

    Update spacing .js file.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -22,10 +22,8 @@ function addRandomGreeting() {
     "你好，世界！",
     "Bonjour le monde!",
   ];
-
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
   // Add it to the page.
   const greetingContainer = document.getElementById("greeting-container");
   greetingContainer.innerText = greeting;
