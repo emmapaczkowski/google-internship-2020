2459318707c585e2d00064fea721aabc1b7a2b89
emmapaczkowski <emmapaczkowski@gmail.com>
2020-06-28 03:54:35 +0000

    Add fist attempt at the main structure of the problem.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,53 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+  
+  int requestDuration = (int) request.getDuration();
+
+  //if the duration of the meeting is over a day, there are no available times
+  if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+    return new ArrayList<TimeRange>();
+  }
+
+  //if the event time is zero or the number of attendees is zero, the whole day is avalible 
+  if(reqest.getDuration() == 0 || request.getAttendees.size() == 0) {
+    return Arrays.asList(TimeRange.WHOLE_DAY);
+  }
+
+  //if there are no events and the request is less than a say, then return the whole day
+  if(events.isEmpty() && requestDuration <= TimeRange.WHOLE_DAY.duration()) {
+    return Arrays.asList(TimeRange.WHOLE_DAY);
+  }
+
+  ArrayList<TimeRange> unavailableTimes = new ArrayList<TimeRange>();
+
+  for(int i=0 ; i < events.length() ; i++) {
+    ArrayList<String> requiredAttendees = new ArrayList<String>();
+    requiredAttendees = request.getAttendees();
+    boolean isRequired == false 
+    while(isvalid == false) {
+      for(int j=0 ; j<requiredAttendees.length() ; j++) {
+        if(events[i].attendees.contain(requiredAttendees[j]) {
+          isRequired = true;
+          addUnavailableTime(event[i].when);
+        }
+      }
+    }
+  }
+
+  }
+
+  public void addUnavailableTime(TimeRange time) {
+      //you need to finish this function
+  }
+
   }
 }
