3d37052e07e17d29c5522f8e044aab51c514b84d
emmapaczkowski <emmapaczkowski@gmail.com>
2020-08-03 20:28:41 +0000

    Itterate through attendees to get those that are mandatory and optional.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,17 +25,43 @@ import java.util.stream.Collectors;
 import java.util.Iterator;
 
 public final class FindMeetingQuery {
-  public ArrayList < TimeRange > unavailableTimes = new ArrayList < TimeRange > ();
+ // public ArrayList < TimeRange > unavailableTimes = new ArrayList < TimeRange > ();
 
   public Collection < TimeRange > query(Collection < Event > events, MeetingRequest request) {
     //if the duration of the meeting is over a day, there are no available times
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
       return Collections.emptyList();
     }
-    //if there are no events and the request is less than a say, then return the whole day
+    //if there are no events and the request is less than a day, then return the whole day
     if (events.isEmpty() && request.getDuration() <= TimeRange.WHOLE_DAY.duration()) {
       return Arrays.asList(TimeRange.WHOLE_DAY);
     }
+
+    // Arraylist to hold event times with all mandatory attendees    
+    List<TimeRange> mandatoryEvents = new ArrayList();
+
+    // Arraylist to hold event times with all attendees, mandatory and optional
+    List<TimeRange> withOptionalEvents = new ArrayList();
+    for (Event event: events) {
+        // Check if attendees from event are requested for meeting
+        for (String attendee : event.getAttendees()) {            
+            // Check that attendee is a mandatoryattendee
+            if ((request.getAttendees().contains(attendee)) ) {
+                mandatoryEvents.add(event.getWhen());
+                withOptionalEvents.add(event.getWhen());
+            }
+            // Check if attendee is an optional attendee
+            else if ((request.getOptionalAttendees().contains(attendee))) {
+                withOptionalEvents.add(event.getWhen()); 
+            }                
+        }
+    }
+
+
+
+
+
+
     //itterate through events to find all that have a conflic with a required attendee.
     //Add those events to the unavailableTimes array.
     ArrayList < String > requiredAttendees = new ArrayList < > (request.getAttendees());
@@ -43,7 +69,7 @@ public final class FindMeetingQuery {
       for (int j = 0; j < requiredAttendees.size(); j++) {
         //If the current event has a required attendee then it is not available, add it to the unavailableTimes Array.
         if (iterator.next().getAttendees().contains(requiredAttendees.get(j))) {
-          addUnavailableTime(cur.getWhen());
+          addUnavailableTime(iterator.next().getWhen());
           break;
         }
       }
